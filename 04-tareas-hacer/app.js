094164f28932a5c9a32006af6231c0a2afa4f998
Raúl Romero <link_2642@hotmail.com>
2022-05-30 07:18:55 -0500

    add: uuid

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,23 @@
+
+require('colors')
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+const Tareas = require('./models/tareas');
+
+
+
+const main = async () => {
+
+    let opt = '';
+    do {
+        opt = await inquirerMenu();
+        console.log({opt});
+      
+        await pausa()
+       
+    } while( opt !== '0');
+   
+   
+}
+
+main();
