fb557650073d540b0e8c09a3eec085c8e699b6b1
Raul Romero <link_2642@hotmail.com>
2023-04-02 12:35:01 -0600

    update: primera respuesta de parte de google

--- 07-restserver/controllers/auth.controller.js
+++ 07-restserver/controllers/auth.controller.js
@@ -50,6 +50,17 @@ const login = async (req, res= response) => {
 
 }
 
+const googleSignIn = async (req, res = response ) => {
+   
+    const {id_token} = req.body;
+
+    res.json({
+        msg: 'todo bien!',
+        id_token
+    })
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
