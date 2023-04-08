80ab1c5ab82c8c5263b7180fa8c60feb9b818bad
Raul Romero <link_2642@hotmail.com>
2023-04-08 10:31:33 -0600

    update: mensajes conexion-desconexion

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,1 +1,19 @@
+//refetrencias html
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+
 const socket = io();
+
+socket.on('connect',() => {
+
+    console.log('conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+socket.on('disconnect',() => {
+
+    console.log('desconectado del servidor');
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+})
