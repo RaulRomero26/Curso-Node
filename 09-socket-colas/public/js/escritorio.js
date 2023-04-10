87c187edbdfe9ab869e39a8de5db3edf8c9c80dc
RaulRomero26 <link_2642@hotmail.com>
2023-04-10 12:02:44 -0600

    update inicio pantalla escritorio

--- 09-socket-colas/public/js/escritorio.js
+++ 09-socket-colas/public/js/escritorio.js
@@ -1,3 +1,42 @@
+//referencias html
+
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button')
 
 
-console.log('Escritorio HTML');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+
+socket.on('ultimo-ticket', (ultimo) => {
+    //console.log( payload )
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+    
+    // socket.emit( 'enviar-mensaje', payload, ( id ) => {
+    //     console.log('Desde el server', id );
+    // });
+
+});
